293a46ffad540cd9be8a025388ddd970bedd5a75
Riboshom <riboshom@hotmail.com>
2016-12-13 08:54:53 -0500

    Added a directive for file trees

--- renderer.js
+++ renderer.js
@@ -5,8 +5,9 @@
 require('angular');
 require('angular-ui-bootstrap');
 const _ =  require('lodash');
+require('angular-ivh-treeview/dist/ivh-treeview.js');
 
-var gitface = angular.module('gitface', ['ui.bootstrap']);
+var gitface = angular.module('gitface', ['ui.bootstrap', 'ivh.treeview']);
 
 gitface.constant('ipc', require('./renderer-ipc-calls.js'));
 
@@ -47,6 +48,29 @@ gitface.controller('repoSelectorCtrl', ["$scope", "ipc", "$uibModalInstance", fu
 
 	$scope.cloneUrl = "";
 }]);
+
+gitface.controller('FileChangesCtrl', ["$scope", "ipc", function($scope, ipc){
+	$scope.unstagedChanges = [{
+		label: 'repo',
+		value: '',
+		children: [{
+			label: 'SomeFile.txt',
+			value: 'SomeDir/SomeFile.txt'
+		},{
+			label: 'EmptyDir',
+			value: 'SomeDir/EmptyDir',
+			children: []
+		}]
+	}];
+}]);
+
+gitface.config(function(ivhTreeviewOptionsProvider) {
+	ivhTreeviewOptionsProvider.set({
+        twistieCollapsedTpl: '<span class="glyphicon glyphicon-chevron-right"></span>',
+        twistieExpandedTpl: '<span class="glyphicon glyphicon-chevron-down"></span>',
+        twistieLeafTpl: '&#9679;'
+	});
+})
 
 const path = require('path');
 const url = require('url');
