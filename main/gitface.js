e1fa1ed529f2a13be22a1931f15ad144c97414a5
Riboshom <riboshom@hotmail.com>
2017-01-09 21:02:23 -0500

    Backend code to fetch a commit by ref and a commit's ancestors

--- main/gitface.js
+++ main/gitface.js
@@ -6,7 +6,7 @@ module.exports = function(electron, app, mainWindow) {
 	const NodeGit = require("nodegit");
 	const Repository = require("./repository.js")
 
-	let repo = undefined;
+	var repo = undefined;
 
 	ipc.on('open-directory-picker', function(ev) {
 		var filePaths = dialog.showOpenDialog(mainWindow, {
@@ -26,6 +26,28 @@ module.exports = function(electron, app, mainWindow) {
 
 		repo.testRepo().then(function(repoData) {
 			ev.sender.send('changed-directory', repoData)
+		});
+	})
+
+	ipc.on('get-files', function(ev) {
+
+	})
+
+	ipc.on('get-head-ref', function(ev) {
+		// Either a commit (headless) or a branch ref, but always a "reference" object
+		repo.getHead().then(function(headRef){
+			ev.sender.send('reply-head-ref', headRef.toString());
+		});
+	})
+
+	// First commit should be the last commit we know, so we get the current head at least once,
+	// then the renderer should query for the last commit we sent it.
+	ipc.on('get-commit-chain', function(ev, firstCommit, rangeLimit) {
+		console.log("Got message")
+		repo.getCommitChain(firstCommit, rangeLimit).then(function(serializedCommitList){
+			ev.sender.send('reply-commit-chain', serializedCommitList)
+		}).catch(function(e){
+			console.error(e);
 		});
 	})
 
--- main/repository.js
+++ main/repository.js
@@ -2,6 +2,25 @@ const fs = require('fs');
 const Promise = require('promise')
 const NodeGit = require("nodegit");
 
+// Notes about references :
+// Check the manpage for git-rev-parse
+// 'HEAD' : Current commit of the working tree
+// Depending on the current state of the repo, there may also be 'FETCH_HEAD', 'ORIG_HEAD', 'MERGE_HEAD' or 'CHERRY_PICK_HEAD'
+// <sha1> : Commit hash
+// <refname> : [[refs/]heads/]master
+// <refname>@{ordinal} : Points to a certain commit in reference to <refname>
+// @{ordinal} : Points to a certain commit in reference to HEAD
+// @ : Points to HEAD
+// <ref>^<n> : nth parent of commit (on a merge, we count both parents as ^<x> and ^<x+1>)
+// <ref>^^ : Equivalent to <ref>^1^1 or <ref>~2
+// <ref>~<n> : nth generation parent of commit (only the "first parent" line is accounted fore in a merge)
+
+// Ordinals
+// <branchname>@{upstream}/{u} : Remote head for that branch (pulling location)
+// <branchname>@{push} : Remote head for that branch (pushing location)
+//
+
+
 function Repository(path) {
 	// Only the path is stored. Everything else is transient.
 	this.path = path;
@@ -41,6 +60,59 @@ Repository.prototype.testRepo = function() {
 			isRepo: false,
 		};
 	});
+}
+
+Repository.prototype.getHead = function() {
+	var dirPromise = this.getDirectory();
+
+	return dirPromise.then(NodeGit.Repository.open).then(function(repoObject) {
+		return repoObject.head();
+	})
+}
+
+Repository.prototype.getCommitChain = function(firstCommit, rangeLimit) {
+	var dirPromise = this.getDirectory();
+
+	return dirPromise.then(NodeGit.Repository.open).then(function(repoObject) {
+		var revWalk = NodeGit.Revwalk.create(repoObject);
+		// Sets commit as starting point for revWalk
+		revWalk.pushRef(firstCommit);
+		// Hides nth generation parent of firstCommit and its ancestors
+		revWalk.hideRef(firstCommit + "~" + rangeLimit); // FIXME : Unsafe?
+		console.log("Ready to walk");
+		return revWalk.getCommits();
+	}).then(function(commitList) {
+		var serializedCommitList = [];
+		for (commitObject of commitList) {
+			var nParents = commitObject.parentcount();
+			// Git normally labels its parents so that commit^0 is the commit itself
+			// Libgit considers parent 0 to be the first parent instead.
+			var parentCommits = [];
+
+			// TODO : Replace this bit with .parents() is possible, couldn't get it to work.
+			for (var i = 0; i < nParents; i++) {
+				parentCommits.push(commitObject.parentId(i).tostrS());
+			}
+
+			var sCommit = {
+				hash: commitObject.id(),
+				parents: parentCommits,
+				author: {
+					name: commitObject.author().name(),
+					email: commitObject.author().email(),
+				},
+				committer: {
+					name: commitObject.committer().name(),
+					email: commitObject.committer().email(),
+				},
+				date: commitObject.date(),
+				message: commitObject.message(),
+			}
+
+			serializedCommitList.push(sCommit);
+		}
+		return serializedCommitList;
+	})
 }
 
 module.exports = Repository;
